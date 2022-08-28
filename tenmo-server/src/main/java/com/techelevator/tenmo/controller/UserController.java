6f83dae5fec564600ba4006faa290f817f5ce2e2
Dennis <DennisEdwardConner@gmail.com>
2022-08-27 22:16:12 -0400

    updated controller classes

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/UserController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.User;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@PreAuthorize("isAuthenticated()")
 public class UserController {
     UserDao userDao;
 
